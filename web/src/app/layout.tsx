dab7bc4af1563e28f3936a631a17ae46868c72cc
Txium <669840065@qq.com>
2026-08-25 19:36:35 +0800

    feat: rename app to 灵感画布

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -7,8 +7,8 @@ import "./globals.css";
 import React from "react";
 
 export const metadata: Metadata = {
-    title: "无限画布",
-    description: "一个无限画布创作工具",
+    title: "灵感画布",
+    description: "一个面向创意工作的无限画布创作工具",
 };
 
 export default function RootLayout({
